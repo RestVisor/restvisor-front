66de418435d5a5c092521c32e61fb36f56fa2077
hupog <hugogonzalez798@gmail.com>
2025-03-18 17:52:59 +0000

    translated to english

--- src/types/index.ts
+++ src/types/index.ts
@@ -20,26 +20,23 @@ export interface Table {
   state: string;         
 }
 
-// Interfaz para la tabla `pedidos`
-export interface Pedido {
-  id: number;             // ID del pedido, tipo entero
-  tableNumber: number;    // Número de la mesa relacionada con el pedido
-  dateCreated: Date;      // Fecha de creación del pedido
-  estado: string;         // Estado del pedido (ej. "pendiente", "en preparación", "completado")
+export interface Order {
+  id: number;           
+  tableNumber: number;  
+  dateCreated: Date;     
+  state: string;       
 }
 
-// Interfaz para la tabla `productos`
-export interface Producto {
-  id: number;             // ID del producto, tipo entero
-  name: string;           // Nombre del producto
-  descripcion: string;    // Descripción del producto
-  price: number;          // Precio del producto
+export interface Product {
+  id: number;           
+  name: string;         
+  description: string;   
+  price: number;         
 }
 
-
-export interface DetallePedido {
+export interface OrderDetail {
   id: number;            
-  pedido_id: number;      
-  producto_id: number;  
-  cantidad: number;      
+  order_id: number;      
+  product_id: number;  
+  amount: number;      
 }
